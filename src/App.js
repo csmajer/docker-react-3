3727c4384058625fd0a710d6dfeaafc5d0676690
Csaba Majer <csaba.majer@gmail.com>
2022-06-21 23:02:23 +0200

    changed message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>na ez már valami ccc</p>
+        <p>i was changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
